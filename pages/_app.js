976c2b4fc519b93ef49cf1073bacb313b0bc0de7
Vinícius Rodrigues Godoy dos Santos <v.godoyrodrigues@gmail.com>
2021-07-28 21:57:35 -0300

    Aula 2 finalizada

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
@@ -17,6 +18,29 @@ const theme = {
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Instalura</title>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      
       <GlobalStyle />
       <ThemeProvider theme={theme}>
         <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,9 @@
-import styled from 'styled-components'
-
-// const Title = styled.h1`
-//   font-size: 50px;
-//   color: ${({ theme }) => theme.colors.primary};
-// `
-
-function Title(props) {
-  return <h1>{props.children}</h1>
-}
+import Menu from '../src/componentes/commons/Menu'
 
 export default function Home() {
-  return <div><Title>Qualquer Valor</Title>Hot reload :burn:</div>
+  return (
+    <div>
+      <Menu />
+    </div>
+  )
 }
