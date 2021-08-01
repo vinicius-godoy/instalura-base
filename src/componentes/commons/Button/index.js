527c072f190fdda0f9abf81813235c8b20c88bb4
Vinícius Rodrigues Godoy dos Santos <v.godoyrodrigues@gmail.com>
2021-08-01 16:34:32 -0300

    Aula 5 finalizada

--- src/componentes/commons/Button/index.js
+++ src/componentes/commons/Button/index.js
@@ -1,5 +1,6 @@
 import styled, { css } from 'styled-components';
 import get from 'lodash/get';
+import { TextStyleVariantsMap } from '../../../foundation/Text';
 
 const ButtonGhost = css`
   color: ${(props) => {
@@ -24,6 +25,9 @@ export const Button = styled.button`
   font-weight: bold;
   opacity: 1;
   border-radius: 8px;
+
+  ${TextStyleVariantsMap.smallestException}
+
   ${function(props) {
     console.log('<Button />', props.variant, props.theme, get(props.theme, `colors.${props.variant}.color`));
     if(props.ghost) {
