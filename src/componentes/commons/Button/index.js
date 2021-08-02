66f132f8be6126d0b09c55138f3fdd5a0af68c1f
Vinícius Rodrigues Godoy dos Santos <v.godoyrodrigues@gmail.com>
2021-08-01 21:08:33 -0300

    Aula 7 finalizada

--- src/componentes/commons/Button/index.js
+++ src/componentes/commons/Button/index.js
@@ -2,6 +2,7 @@ import styled, { css } from 'styled-components';
 import get from 'lodash/get';
 import { TextStyleVariantsMap } from '../../../foundation/Text';
 import { breakpointsMedia } from '../../../theme/utils/breakpointsMedia';
+import { propToStyle } from '../../../theme/utils/propToStyle';
 
 const ButtonGhost = css`
   color: ${(props) => {
@@ -49,4 +50,7 @@ export const Button = styled.button`
       ${TextStyleVariantsMap.paragraph1}
     `,
   })}
+
+  ${propToStyle('margin')}
+  ${propToStyle('display')}
 `;
